f69cca137e50f209b1cf3ac3fbc4e0d39f0743bc
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-11-19 23:13:42 +0200

    add microsoft authentication

--- CommonSense.API/Program.cs
+++ CommonSense.API/Program.cs
@@ -1,34 +1,91 @@
+using System.IdentityModel.Tokens.Jwt;
+using CommonSense.API.Middleware;
 using CommonSense.DataAccess;
 using CommonSense.Domain.Interfaces;
 using CommonSense.Domain.Models;
 using CommonSense.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// singleton only for testing
-builder.Services.AddScoped<IRepository<User>, EfCoreUserRepository<User>>();
+builder.Services.AddSwaggerGen(c =>
+{
+    // Enabled OAuth security in Swagger
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+    {
+        new OpenApiSecurityScheme {
+            Reference = new OpenApiReference {
+                Type = ReferenceType.SecurityScheme,
+                Id = "oauth2"
+            },
+            Scheme = "oauth2",
+            Name = "oauth2",
+            In = ParameterLocation.Header
+        },
+        new List <string> ()
+    }
+    });
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow()
+            {
+                AuthorizationUrl = new Uri("https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize"),
+                TokenUrl = new Uri("https://login.microsoftonline.com/consumers/oauth2/v2.0/token"),
+            }
+        }
+    });
+});
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfCoreRepository<>));
+builder.Services.AddScoped<ICommentService, CommentService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IReportService, ReportService>();
 builder.Services.AddDbContext<CommonSenseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("CommonSense.API")));
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+        options.TokenValidationParameters.NameClaimType = "preferred_username";
+    },
+    options => builder.Configuration.Bind("AzureAd", options));
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.OAuthAppName("Swagger Client");
+        options.OAuthClientId(builder.Configuration.GetValue<string>("AzureAd:ClientId"));
+        options.OAuthScopes("openid", "email", "api://7467835d-b0f6-4612-98be-5a3c9ea615ab/email");
+        options.OAuthClientSecret(builder.Configuration.GetValue<string>("AzureAd:Secret"));
+        options.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+    });
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+//app.UseMiddleware<ProfileSetupMiddleware>();
 app.MapControllers();
 
 app.Run();
