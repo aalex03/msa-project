8b984687a4e8145e52b90084a5829cb8a996287d
shigaisen3 <razvan.litzemberger@student.upt.ro>
2024-11-20 22:12:08 +0200

    ADDED POSTS

--- commonsense-frontend/src/App.js
+++ commonsense-frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from '../src/components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import { useIsAuthenticated, useMsal } from "@azure/msal-react";
 import { loginRequest } from "./API/authConfig";
@@ -8,11 +8,15 @@ import { loginRequest } from "./API/authConfig";
 function App() {
   const { instance } = useMsal();
   const isAuthenticated = useIsAuthenticated();
+  const navigate = useNavigate(); // Initialize the useNavigate hook
 
   const signIn = async () => {
     try {
       const loginResponse = await instance.loginPopup(loginRequest);
       sessionStorage.setItem("user", JSON.stringify(loginResponse.account));
+
+      // Redirect to home after successful login
+      navigate("/");
     } catch (error) {
       console.error("Login failed: ", error);
     }
